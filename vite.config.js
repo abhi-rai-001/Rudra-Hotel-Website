2597733b6405a5fb802517b3ac43ec55478644c5
Abhinav Rai <abhi@Abhinavs-MacBook-Air.local>
2025-02-18 02:13:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Rudra-Hotel-Website/',
+  base: "/Rudra-Hotel-Website",
 })
